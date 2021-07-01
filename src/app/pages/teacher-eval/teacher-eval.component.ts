ec0765d3f4aa95cc1e4b4962957d2196d04c5113
KH0RE <cet.quinaluisa@yavirac.edu.ec>
2021-06-30 20:31:27 -0500

    Estructura

---
+++ src/app/pages/teacher-eval/teacher-eval.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-eval',
+  templateUrl: './teacher-eval.component.html',
+  styleUrls: ['./teacher-eval.component.css']
+})
+export class TeacherEvalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
