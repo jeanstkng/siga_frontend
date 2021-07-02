364ea8fa8be8d4d9d6d711722ba5f31dc1e36d79
Evelyn <eyy.faz@yavirac.edu.ec>
2021-07-01 22:58:32 -0500

    as

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,7 +19,7 @@ export const environment = {
     API_URL_COMMUNITY: URL_API + 'community/',
     SYSTEM_ID: 1,
     CLIENT_ID: '1',
-    CLIENT_SECRET: '0Kt3nyrbEMvbyZKisOEiJrLv7EEwcDXpiOZdRdcO',
+    CLIENT_SECRET: 'fqpWoNgmNCgpyqs5yV5sYRBaerNNDl3IJJQJ6hQu',
     GRANT_TYPE: 'password',
 
     SITE_KEY: 'QpKtUkaJ40HNd1Di74B0pKC6asgKzAxuTx69L9Nb',
