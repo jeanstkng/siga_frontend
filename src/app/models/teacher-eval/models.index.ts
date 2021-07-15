608556f3898db77bf5761e77a3b78b8a52bce3ad
unknown <crtituana@gmail.com>
2021-07-14 23:19:20 -0500

    cambios modelos

--- src/app/models/teacher-eval/models.index.ts
+++ src/app/models/teacher-eval/models.index.ts
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 export {Question} from './question';
-=======
 export { DetailEvaluation } from './detail-evaluation';
 export { Evaluation } from './evaluation';
 export { EvaluationType } from './evaluation-type';
->>>>>>> origin/u_10_quinaluisa-david
+
