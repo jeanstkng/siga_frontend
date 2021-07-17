8186036daf1ea3c68bb4bc097e2e303555aeb35b
Joel <joelts159@gmail.com>
2021-07-16 19:07:27 -0500

    deleted componen listgrid

--- src/app/pages/job-board/landing-page/landing-page.module.ts
+++ src/app/pages/job-board/landing-page/landing-page.module.ts
@@ -43,7 +43,6 @@ import {WebProfessionalComponent} from './web-professional/web-professional.comp
 import { OfferDataViewComponent } from './web-offer/offer-data-view/offer-data-view.component';
 import {TotalComponent} from './total/total.component';
 import {ProfessionalListComponent} from './web-professional/professional-list/professional-list.component';
-import { DataViewGridItemComponent } from './web-offer/offer-data-view/data-view-grid-item/data-view-grid-item.component';
 import { DataViewListItemComponent } from './web-offer/offer-data-view/data-view-list-item/data-view-list-item.component';
 import {DividerModule} from 'primeng/divider';
 import {CategoryFilterComponent} from './web-professional/category-filter/category-filter.component';
@@ -98,7 +97,6 @@ import {FilterComponent} from './web-professional/filter/filter.component';
         CategoryFilterComponent,
         FilterComponent,
         OfferDataViewComponent,
-        DataViewGridItemComponent,
         DataViewListItemComponent,
 
     ],
